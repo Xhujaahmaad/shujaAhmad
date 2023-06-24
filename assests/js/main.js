593b57bb56c500b47d75885bcd14b7ee50894350
Shuja <shujaahmad581@gmail.com>
2023-06-25 03:22:00 +0500

    add

--- assests/js/main.js
+++ assests/js/main.js
@@ -357,27 +357,3 @@ $(document).ready(function () {
 });
 
 // testimonials slider
-
-var swiper = new Swiper(".mySwiper", {
-  loop: false,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  breakpoints: {
-    // when window width is >= 320px
-    320: {
-      slidesPerView: 1,
-      spaceBetween: 20,
-    },
-    // when window width is >= 640px
-    640: {
-      slidesPerView: 2,
-      spaceBetween: 30,
-    },
-    1100: {
-      slidesPerView: 3,
-      spaceBetween: 30,
-    },
-  },
-});
